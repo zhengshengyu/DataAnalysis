6ebb81083746f73e61c81a80d9ab694ff6563f7e
zhengshngyu <zhengshy2015@gmail.com>
2017-04-23 11:54:43 +0800

    first commit

---
+++ pydata-book-master/ch05.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+s1 = Series([7.3, -2.5, 3.4, 1.5], index=['a', 'c', 'd', 'e'])
+s2 = Series([-2.1, 3.6, -1.5, 4, 3.1], index=['a', 'c', 'e', 'f', 'g'])
+
+print s1 + s2
